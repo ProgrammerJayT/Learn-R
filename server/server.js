1a81e3046982f7cd154f4a0071bc1e8d54c0c7d5
Moobi Kabelo <giftmoobi@gmail.com>
2022-07-31 01:01:40 +0200

    added server starter, student routes

---
+++ server/server.js
@@ -0,0 +1,44 @@
+/**
+ * @desc -
+ */
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import dotenv from 'dotenv';
+import express from 'express';
+import helmet from 'helmet';
+import ip from 'ip';
+// eslint-disable-next-line import/extensions
+import { errorLog, successLog } from './utils/logger.js';
+import database from './libs/database';
+import studentRoutes from './routes/student.routes';
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(express.json());
+app.use(helmet());
+app.use(
+  cors({
+    origin: '*',
+    credentials: true,
+  }),
+);
+
+database.mongo
+  .then(() => {
+    successLog.info('Connected to MongoDB');
+  })
+  .catch((MongoError) => {
+    errorLog.error(`MongoDB connection error: ${MongoError}`);
+  });
+
+app.use('/api/student', studentRoutes);
+
+app.listen(PORT, () => {
+  successLog.info(`Server Started: http://${ip.address()}:${PORT}`);
+});
