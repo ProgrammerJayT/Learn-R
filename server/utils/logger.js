04ff86ea0af5e77cc68c1f56882a0946e3b0692d
Moobi Kabelo <giftmoobi@gmail.com>
2022-07-31 00:59:17 +0200

    added system logger

--- server/utils/logger.js
+++ server/utils/logger.js
@@ -1,41 +1,37 @@
 /**
- * @author: Moobi Kabelo <@elregalo>
+ * @desc -
  */
 import 'winston-daily-rotate-file';
 import winston from 'winston';
 
-class SystemLogger {
-  error = winston.createLogger({
-    name: 'error',
-    level: 'error',
-    json: true,
-    transports: [
-      new winston.transports.DailyRotateFile({
-        filename: './logs/error-%DATE%.log',
-        datePattern: 'YYYY-MM-DD-HH',
-        zippedArchive: true,
-        maxSize: '20m',
-        maxFiles: '14d',
-      }),
-    ],
-  });
+/* Creating a logger for errors */
+export const errorLog = winston.createLogger({
+  name: 'error',
+  level: 'error',
+  json: true,
+  transports: [
+    new winston.transports.DailyRotateFile({
+      filename: './logs/error-%DATE%.log',
+      datePattern: 'YYYY-MM-DD-HH',
+      zippedArchive: true,
+      maxSize: '20m',
+      maxFiles: '14d',
+    }),
+  ],
+});
 
-  success = winston.createLogger({
-    name: 'success',
-    level: 'info',
-    json: true,
-    transports: [
-      new winston.transports.DailyRotateFile({
-        filename: './logs/success-%DATE%.log',
-        datePattern: 'YYYY-MM-DD-HH',
-        zippedArchive: true,
-        maxSize: '20m',
-        maxFiles: '14d',
-      }),
-    ],
-  });
-}
-
-const logger = new SystemLogger();
-
-export default logger;
+/* Creating a logger for success messages */
+export const successLog = winston.createLogger({
+  name: 'success',
+  level: 'info',
+  json: true,
+  transports: [
+    new winston.transports.DailyRotateFile({
+      filename: './logs/success-%DATE%.log',
+      datePattern: 'YYYY-MM-DD-HH',
+      zippedArchive: true,
+      maxSize: '20m',
+      maxFiles: '14d',
+    }),
+  ],
+});
